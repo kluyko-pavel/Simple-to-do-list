6bf39c7ede9c1a5a4dd5e0f0bb9c015467445f78
kluyko-pavel <kluyko.pasha@mail.ru>
2023-03-02 08:33:47 +0300

    fix

--- scripts/components/toggleThemeBtn.js
+++ scripts/components/toggleThemeBtn.js
@@ -2,10 +2,5 @@ export function createToggleThemeBtn () {
   const themeBtn = document.createElement('button')
   themeBtn.className = 'toggle-theme-btn'
   themeBtn.type = 'button'
-  themeBtn.style.backgroundImage = 'url(../../assets/theme-icon.png)'
-  themeBtn.style.backgroundSize = 'contain'
-  themeBtn.style.backgroundRepeat = 'no-repeat'
-  themeBtn.style.backgroundPosition = 'center'
-  themeBtn.style.width = '35px'
   return themeBtn
 }
